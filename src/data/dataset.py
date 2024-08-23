bcba52ae1a0764601d152f9c5ea6c13023ec47ee
Xiaorui Dong <dxr1234@gmail.com>
2024-08-23 02:01:56 -0400

    Add the ESOL dataset available in PyG
    atom features in the dataset are long type. Created a ESOLDataset to force converting data.x to float for operation compatibility.

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -8,10 +8,14 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 
+from torch_geometric.datasets import MoleculeNet
+
 
 _random_data_path = (
     Path(__file__).absolute().parents[2] / "data" / "random_1dfeature_dataset.csv"
 )
+
+_graph_data_path = Path(__file__).absolute().parents[2] / "data" / "esol"
 
 
 class CSVDataset(Dataset):
@@ -30,3 +34,26 @@ class CSVDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.inputs[idx], self.targets[idx]
+
+
+class ESOLDataset(MoleculeNet):
+    """A customized Dataset for loading ESOL dataset, so that
+    node features are float"""
+
+    def __init__(self, root: str | Path = _graph_data_path):
+        super().__init__(root=root, name="esol")
+
+    def __getitem__(self, idx):
+        data = super().__getitem__(idx)
+        data.x = data.x.float()  # force the feature to be float for compatibility
+        return data
+
+
+# def load_esol_dataset():
+#     """
+#     A helper function to load ESOL dataset.
+#     """
+#     dataset = MoleculeNet(root=_graph_data_path, name="esol")
+#     for data in dataset:
+#         data.x = data.x.float()  # force the feature to be float for compatibility
+#     return dataset
