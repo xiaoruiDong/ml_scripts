7d2dfbda40947a44f4024e22d1e534e0595ef2bf
Xiaorui Dong <dxr1234@gmail.com>
2024-08-22 20:49:19 -0400

    Autopep8: dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -9,21 +9,24 @@ import torch
 from torch.utils.data import Dataset
 
 
-_random_data_path = Path(__file__).absolute().parents[2] / 'data' / 'random_1dfeature_dataset.csv'
+_random_data_path = (
+    Path(__file__).absolute().parents[2] / "data" / "random_1dfeature_dataset.csv"
+)
 
 
 class CSVDataset(Dataset):
     """A customized Dataset for loading data from a csv file"""
+
     def __init__(
         self,
         path: str | Path = _random_data_path,
     ):
         df = pd.read_csv(path)
-        self.targets = torch.tensor(df['y'].values, dtype=torch.float32)
-        self.inputs = torch.tensor(df.drop('y', axis=1).values, dtype=torch.float32)
-    
+        self.targets = torch.tensor(df["y"].values, dtype=torch.float32)
+        self.inputs = torch.tensor(df.drop("y", axis=1).values, dtype=torch.float32)
+
     def __len__(self):
         return len(self.targets)
-    
+
     def __getitem__(self, idx):
         return self.inputs[idx], self.targets[idx]
